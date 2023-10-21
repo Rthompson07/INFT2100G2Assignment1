b73ee0a83f003e3007c9567e1ccf718cb9a0b6c1
Rhys Thompson <rhys.thompson@dcmail.ca>
2023-10-20 22:29:58 -0400

    worked on dashboard again

--- sign-in.php
+++ sign-in.php
@@ -97,8 +97,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <?php
         if(isset($user)) {
             print_r($user);
-        } else {
-            echo('not user');
         }
 
         if(hasFlashMessage()){
